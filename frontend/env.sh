d08d368463197fe2665459f2e67702c5532ad6d6
moddroid94 <ilsecco94@gmail.com>
2026-02-26 19:11:18 +0100

    fix(frontend): fixed updating config.ts to allow for custom host

--- frontend/env.sh
+++ frontend/env.sh
@@ -3,8 +3,13 @@
 for i in $(env | grep '^TERA_'); do
   key=$(echo $i | cut -d '=' -f 1)
   value=$(echo $i | cut -d '=' -f 2-)
-  echo "Replacing $key with $value"
+  echo "Replacing static $key with $value"
   find /app/dist -type f -name '*.js' -exec sed -i "s|${key}|${value}|g" '{}' +
 done
-
+for i in $(env | grep '^TERA_'); do
+  key=$(echo $i | cut -d '=' -f 1)
+  value=$(echo $i | cut -d '=' -f 2-)
+  echo "Replacing config $key with $value"
+  find /app -type f -name 'vite.config.ts' -exec sed -i "s|${key}|${value}|g" '{}' +
+done
 exec npm run preview
