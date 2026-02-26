9e8f1d15018f3fc29ca2a21979136faa77456032
moddroid94 <ilsecco94@gmail.com>
2026-02-26 18:30:05 +0100

    fix(frontend): fix for entrypoint command

--- frontend/env.sh
+++ frontend/env.sh
@@ -1,15 +1,8 @@
-#!/usr/bin/env sh
-
-# Set the exit flag to exit immediately if any command fails
-set -e
-
-
-# Iterate through each environment variable that starts with APP_PREFIX
-env | grep "^${TERA_}" | while IFS='=' read -r key value; do
-    # Display the variable being replaced
-    echo "  • Replacing ${key} → ${value}"
-
-    # Use find and sed to replace the variable in all files within the directory
-    find "/app" -type f -name 'vite.config.ts' \
-        -exec sed -i "s|${key}|${value}|g" {} +
-done
+#!/bin/sh
+# Replace placeholders in JS files with actual env vars
+for i in $(env | grep '^TERA_'); do
+  key=$(echo $i | cut -d '=' -f 1)
+  value=$(echo $i | cut -d '=' -f 2-)
+  echo "Replacing $key with $value"
+  find /app -type f -name 'vite.config.ts' -exec sed -i "s|${key}|${value}|g" '{}' +
+done   
